1a9b90931a15f638c1a11417366bb8d45fd00557
sanguine <sanguine.rose.57@gmail.com>
2024-02-03 11:56:58 -0700

    changed theme for rose-pine to duskfox for alacritty and nvim

--- nvim/lua/sanguine/plugins/colorscheme.lua
+++ nvim/lua/sanguine/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-  "rose-pine/nvim",
-  name = "rose-pine",
+  -- "rose-pine/nvim",
+  -- name = "rose-pine",
+  "EdenEast/nightfox.nvim",
+  name = "nightfox",
   priority = 1000, -- load before other plugins
   config = function()
     -- load colorsheme
-    vim.cmd.colorscheme "rose-pine"
+    -- vim.cmd.colorscheme "rose-pine"
+    vim.cmd.colorscheme "duskfox"
   end,
 }
